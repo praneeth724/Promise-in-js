435506d889b0d7ba370b478f70908b76e1b0c30c
kavindu praneeth <116500216+praneeth724@users.noreply.github.com>
2024-08-05 17:33:41 +0530

    Update PromiseAny.js

--- Promises/PromiseAny.js
+++ Promises/PromiseAny.js
@@ -1,15 +1,17 @@
-// const promise1 = Promise.reject('Error 1');
-// const promise2 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 100, 'Success');
-// });
-// const promise3 = Promise.reject('Error 2');
+//Succcess State
+const promise1 = Promise.reject('Error 1');
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'Success');
+});
+const promise3 = Promise.reject('Error 2');
 
-// Promise.any([promise1, promise2, promise3]).then((value) => {
-//   console.log(value); // 'Success'
-// }).catch((error) => {
-//   console.error(error);
-// });
+Promise.any([promise1, promise2, promise3]).then((value) => {
+  console.log(value); // 'Success'
+}).catch((error) => {
+  console.error(error);
+});
 
+//Reject State
 const promise1 = Promise.reject('Error 1');
 const promise2 = Promise.reject('Error 2');
 const promise3 = Promise.reject('Error 3');
