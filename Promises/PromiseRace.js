4f9d71328a86b1d2fa96df084603b9699510f3d0
kavindu praneeth <116500216+praneeth724@users.noreply.github.com>
2024-08-05 17:38:47 +0530

    Update PromiseRace.js

--- Promises/PromiseRace.js
+++ Promises/PromiseRace.js
@@ -1,15 +1,16 @@
-// const promise1 = new Promise((resolve, reject) => {
-//     setTimeout(resolve, 500, 'one');
-//   });
+//Success State
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 'one');
+  });
   
-//   const promise2 = new Promise((resolve, reject) => {
-//     setTimeout(resolve, 100, 'two');
-//   });
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'two');
+  });
   
-//   Promise.race([promise1, promise2]).then((value) => {
-//     console.log(value); // 'two'
-//   });
-  
+  Promise.race([promise1, promise2]).then((value) => {
+    console.log(value); // 'two'
+  });
+  //Reject State
 const promise1 = new Promise((resolve, reject) => {
     setTimeout(reject, 500, 'Error from promise1');
   });
