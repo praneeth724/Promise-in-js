8b43ecc06fb7ebed377a82f39f08c5adc86d4cfa
kavindu praneeth <116500216+praneeth724@users.noreply.github.com>
2024-08-05 17:32:04 +0530

    Update PromiseAll.js

--- Promises/PromiseAll.js
+++ Promises/PromiseAll.js
@@ -1,13 +1,15 @@
-// const promise1 = Promise.resolve(3);
-// const promise2 = 42;
-// const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 100, 'foo');
-// });
+//success state
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
 
-// Promise.all([promise1, promise2, promise3]).then((values) => {
-//   console.log(values); // [3, 42, 'foo']
-// });
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values); // [3, 42, 'foo']
+});
 
+//reject state
 const promise1 = Promise.resolve(3);
 const promise2 = Promise.reject('Error');
 const promise3 = new Promise((resolve, reject) => {
